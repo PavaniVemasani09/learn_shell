303ac93c0091c8b702628c8ac5e69c51050e5a91
Pavani <vemasani.pavani@gmail.com>
2024-03-04 16:10:21 +0530

    remove the app before executing backend file

--- backend.sh
+++ backend.sh
@@ -4,6 +4,8 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
 cp backend.service /etc/systemd/system/backend.service
+#remove the /app by using below cmd before executing .i.e first iteration will work ,next iterations it wont work.
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
