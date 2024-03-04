03e03a8d4278d97188ebaf530cde819da040e647
Pavani <vemasani.pavani@gmail.com>
2024-03-04 16:52:45 +0530

    Adding Redirectories

--- backend.sh
+++ backend.sh
@@ -1,21 +1,21 @@
 #scriptpath="$PWD"
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-useradd expense
-cp backend.service /etc/systemd/system/backend.service
+dnf module disable nodejs -y>/tmp/expense.log
+dnf module enable nodejs:20 -y>/tmp/expense.log
+dnf install nodejs -y>/tmp/expense.log
+useradd expense>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service>/tmp/expense.log
 #remove the /app by using below cmd before executing .i.e first iteration will work ,next iterations it wont work.
-rm -rf /app
-mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
+rm -rf /app>/tmp/expense.log
+mkdir /app>/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip>/tmp/expense.log
+cd /app>/tmp/expense.log
 
-unzip  /tmp/backend.zip
-cd /app
-npm install
+unzip  /tmp/backend.zip>/tmp/expense.log
+cd /app>/tmp/expense.log
+npm install>/tmp/expense.log
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
-dnf install mysql -y
-mysql -h 172.31.4.183 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+systemctl daemon-reload>/tmp/expense.log
+systemctl enable backend>/tmp/expense.log
+systemctl start backend>/tmp/expense.log
+dnf install mysql -y>/tmp/expense.log
+mysql -h 172.31.4.183 -uroot -pExpenseApp@1 < /app/schema/backend.sql>/tmp/expense.log
