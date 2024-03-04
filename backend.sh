8a4ce055801bfd351cb100bf95eac180a63ff3c2
Pavani <vemasani.pavani@gmail.com>
2024-03-04 14:59:44 +0530

    display the content

--- backend.sh
+++ backend.sh
@@ -3,16 +3,15 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
-#rm -rf /app
+cp backend.service /etc/systemd/system/backend.service
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
-#unzip -o /tmp/backend.zip
+
 unzip  /tmp/backend.zip
 cd /app
 npm install
-#cp $scriptpath/backend.service /etc/systemd/system/backend.service
-cp backend.service /etc/systemd/system/backend.service
+
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
