eb685ac7300939690130e7021c5a803e70da26c6
Pavani <vemasani.pavani@gmail.com>
2024-03-04 11:45:08 +0530

    create frontend script

---
+++ frontend.sh
@@ -0,0 +1,8 @@
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+vim /etc/nginx/default.d/expense.conf
+systemctl restart nginx
