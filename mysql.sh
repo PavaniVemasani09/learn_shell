a6dc18674744d9e7c053f179e782871962733bd1
Pavani <vemasani.pavani@gmail.com>
2024-03-05 23:38:47 +0530

    Adding messages to the mysql

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,17 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_root_passwd=$1
+if [ -z "${mysql_root_passwd}" ];then
+  echo "Password is missing"
+  exit -e "\e[31mPassword is missing\e[0m"
+  fi
+printHeading "Install Mysql server"
+dnf install mysql-server -y &>>LOG
+checkStatus $?
+
+printHeading "Enable and start the server"
+systemctl enable mysqld &>>LOG
+systemctl start mysqld &>>LOG
+checkStatus $?
+
+printHeading "Install nginx"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOG
+checkStatus $?
