8c34417cb21d8759d8548e09230e863fccf6b994
Pavani <vemasani.pavani@gmail.com>
2024-03-11 21:50:58 +0530

    backend server and service file

--- expense_backend.sh
+++ expense_backend.sh
@@ -1,6 +1,6 @@
-dnf install nodejs -y
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
+dnf install nodejs -y
 useradd expense
 cp expense_backend.service /etc/systemd/system/backend.service
 rm -rf /app
