9202e6d5a0127f30375f1bf93b11d290c72d260b
Pavani <vemasani.pavani@gmail.com>
2024-03-11 21:20:11 +0530

    create mysql server

--- expense_mysql.sh
+++ expense_mysql.sh
@@ -1,10 +1,4 @@
-dnf install nodejs -y
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-useradd expense
-mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
-unzip /tmp/backend.zip
-cd /app
-npm install
+dnf install mysql-server -y
+systemctl enable mysqld
+systemctl start mysqld
+mysql_secure_installation --set-root-pass ExpenseApp@1
