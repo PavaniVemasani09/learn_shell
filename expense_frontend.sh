9178664cace3aba0450a54b1a49d006641639bb2
Pavani <vemasani.pavani@gmail.com>
2024-03-13 23:32:00 +0530

    replaced private Ip address

--- expense_frontend.sh
+++ expense_frontend.sh
@@ -8,13 +8,16 @@ systemctl enable nginx &>>LOG
 systemctl start nginx &>>LOG
 print_status $?
 
+print_heading "copy configuration into nginx config file"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>LOG
+print_status $?
+
 print_heading "remove default content in an nginx file"
 rm -rf /usr/share/nginx/html/* &>>LOG
 print_status $?
 
-print_heading "copy configuration into nginx config file"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>LOG
-print_status $?
+
+
 print_heading "Download frontend zip  file"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>LOG
 print_status $?
