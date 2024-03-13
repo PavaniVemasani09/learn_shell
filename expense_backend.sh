4af8d6c45652845257f17f85113f2766ac6a5f64
Pavani <vemasani.pavani@gmail.com>
2024-03-13 10:13:55 +0530

    Dont repeat same steps call a function once

--- expense_backend.sh
+++ expense_backend.sh
@@ -1,68 +1,73 @@
-echo "Password should not be hardcoded"
+print_heading()
+{
+  echo $1
+  echo "################" $1 "##############" &>>/tmp/expense.log
+}
+print_heading "Password should not be hardcoded"
 mysql_root_password=$1
-echo "disable and enable backend service"
+print_heading "disable and enable backend service"
 dnf module disable nodejs -y &>>/tmp/expense.log
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
-echo "To know success or failure"
+print_heading "To know success or failure"
 echo $?
 
 echo "Install nodejs"
 dnf install nodejs -y &>>/tmp/expense.log
-echo "To know success or failure"
+print_heading "To know success or failure"
 echo $?
 
-echo "Add application user"
+print_heading "Add application user"
 useradd expense &>>/tmp/expense.log
-echo "To know success or failure"
+print_heading "To know success or failure"
 echo $?
 
-echo "Copy backend service file into another folder"
+print_heading "Copy backend service file into another folder"
 cp expense_backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
-echo "To know success or failure"
+print_heading "To know success or failure"
 echo $?
 
 
-echo "remove directory when we iterate second time"
+print_heading "remove directory when we iterate second time"
 rm -rf /app &>>/tmp/expense.log
 echo $?
 
-echo "make directory app"
+print_heading "make directory app"
 mkdir /app &>>/tmp/expense.log
 echo $?
 
-echo "Download backend code"
+print_heading "Download backend code"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
 
-echo "Move to app directory"
+print_heading "Move to app directory"
 cd /app &>>/tmp/expense.log
 echo $?
 
-echo "unzip backend code"
+print_heading "unzip backend code"
 unzip /tmp/backend.zip &>>/tmp/expense.log
 echo $?
 
-echo "move to app directory"
+print_heading "move to app directory"
 cd /app &>>/tmp/expense.log
 echo $?
 
-echo "Install npm package for dependencies"
+print_heading "Install npm package for dependencies"
 npm install &>>/tmp/expense.log
 echo $?
 
-echo "Reload backend service file"
+print_heading "Reload backend service file"
 systemctl daemon-reload &>>/tmp/expense.log
 echo $?
 
-echo "Start service"
+print_heading "Start service"
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
 echo $?
 
-echo "Install mysql server "
+print_heading "Install mysql server "
 dnf install mysql -y &>>/tmp/expense.log
 echo $?
 
-echo "Load schema"
+print_heading "Load schema"
 mysql -h 172.31.5.158 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>/tmp/expense.log
 echo $?
