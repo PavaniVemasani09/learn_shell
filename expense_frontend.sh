04ed6866dc302243a494b9bc7d6a44efbc89b5fd
Pavani <vemasani.pavani@gmail.com>
2024-03-13 23:06:28 +0530

    frontend code

--- expense_frontend.sh
+++ expense_frontend.sh
@@ -32,13 +32,4 @@
 #systemctl restart nginx &>>LOG
 #print_status $?
 
-dnf install nginx -y &>>/tmp/expense.log
-systemctl enable nginx  &>>/tmp/expense.log
-systemctl start nginx  &>>/tmp/expense.log
-cp expense.conf /etc/nginx/default.d/expense.conf  &>>/tmp/expense.log
-rm -rf /usr/share/nginx/html/*  &>>/tmp/expense.log
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip  &>>/tmp/expense.log
-cd /usr/share/nginx/html  &>>/tmp/expense.log
-unzip /tmp/frontend.zip  &>>/tmp/expense.log
-systemctl restart nginx   &>>/tmp/expense.log
 
