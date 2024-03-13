739523b846804bcf67c7e1d2d28ffe4329507425
Pavani <vemasani.pavani@gmail.com>
2024-03-13 10:47:02 +0530

    added heading message for mysql

--- expense_mysql.sh
+++ expense_mysql.sh
@@ -1,13 +1,13 @@
 source common_print.sh
 print_heading "Install Mysql server"
-dnf install mysql-server -y
+dnf install mysql-server -y &>>/tmp/expense.log
 print_status
 
 print_heading "start mysql service"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>/tmp/expense.log
+systemctl start mysqld &>>/tmp/expense.log
 print_status
 
 print_heading "set root password"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass print_root_password &>>/tmp/expense.log
 print_status
