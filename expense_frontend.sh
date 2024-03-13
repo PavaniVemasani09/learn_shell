f9b31ff2550550703c8eb0c993e3574cda6623f6
Pavani <vemasani.pavani@gmail.com>
2024-03-13 22:19:30 +0530

    keep the code in a common file

--- expense_frontend.sh
+++ expense_frontend.sh
@@ -10,27 +10,24 @@ systemctl enable nginx &>>$LOG
 systemctl start nginx &>>$LOG
 print_status $?
 
-#print_heading "clean default content in an nginx file"
-#rm -rf ${app_dir}/* &>>$LOG
-#print_status $?
-App_task_code
+print_heading "clean default content in an nginx file"
+rm -rf ${app_dir}/* &>>$LOG
+print_status $?
 
 print_heading "copy configuration into nginx config file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
 print_status $?
 
+print_heading "Download frontend zip  file"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
+print_status $?
 
-App_task_code
-#print_heading "Download frontend zip  file"
-#curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
-#print_status $?
-
-print_heading "move to app directory"
-cd /app &>>$LOG
+print_heading "move to nginx html directory"
+cd ${app_dir} &>>$LOG
 print_status $?
 
 print_heading "unzip frontend zip file"
-unzip /tmp/${component}.zip &>>$LOG
+unzip /tmp/frontend.zip &>>$LOG
 print_status $?
 
 print_heading "Load the changes of configuration file"
