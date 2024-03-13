ce9ffd620d7630d8804e6f846c1c74f6b470e04f
Pavani <vemasani.pavani@gmail.com>
2024-03-13 21:18:24 +0530

    repeated code keep in one common file

--- expense_backend.sh
+++ expense_backend.sh
@@ -1,5 +1,7 @@
 source common_print.sh
 mysql_root_password=$1
+app_dir=/app
+component=backend
 if [ -z  "${mysql_root_password}" ];then
   print_heading "Input status missing"
   exit 1
@@ -23,29 +25,30 @@ cp expense_backend.service /etc/systemd/system/backend.service &>>$LOG
 print_status $?
 
 
-print_heading "remove directory when we iterate second time"
-rm -rf /app &>>$LOG
-print_status $?
+#print_heading "clean directory when we iterate second time"
+#rm -rf /app &>>$LOG
+#print_status $?
 
 print_heading "make directory app"
 mkdir /app &>>$LOG
 print_status $?
 
-print_heading "Download backend code"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$LOG
-print_status $?
+App_task_changes
+#print_heading "Download backend code"
+#curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$LOG
+#print_status $?
 
-print_heading "Move to app directory"
-cd /app &>>LOG
-print_status $?
+#print_heading "Move to app directory"
+#cd ${app_dir} &>>LOG
+#print_status $?
 
-print_heading "unzip backend code"
-unzip /tmp/backend.zip &>>$LOG
-print_status $?
+#print_heading "unzip backend code"
+#unzip /tmp/backend.zip &>>$LOG
+#print_status $?
 
-print_heading "move to app directory"
-cd /app &>>$LOG
-print_status $?
+#print_heading "move to app directory"
+#cd /app &>>$LOG
+#print_status $?
 
 print_heading "Install npm package for dependencies"
 npm install &>>$LOG
--- expense_frontend.sh
+++ expense_frontend.sh
@@ -1,4 +1,6 @@
 source common_print.sh
+app_dir=/usr/share/nginx/html
+component=frontend
 print_heading "Install Nginx"
 dnf install nginx -y &>>$LOG
 print_status $?
@@ -8,25 +10,11 @@ systemctl enable nginx &>>$LOG
 systemctl start nginx &>>$LOG
 print_status $?
 
-print_heading "remove default content in an nginx file"
-rm -rf /usr/share/nginx/html/* &>>$LOG
-print_status $?
-
 print_heading "copy configuration into nginx config file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
 print_status $?
 
-print_heading "Download frontend zip  file"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
-print_status $?
-
-print_heading "move to nginx html directory"
-cd /usr/share/nginx/html &>>$LOG
-print_status $?
-
-print_heading "unzip frontend zip file"
-unzip /tmp/frontend.zip &>>$LOG
-print_status $?
+App_task_changes
 
 print_heading "Load the changes of configuration file"
 systemctl restart nginx &>>$LOG
