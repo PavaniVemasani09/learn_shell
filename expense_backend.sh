b6296c9b3e41f76ed37b6359a34834f56c3af0a3
Pavani <vemasani.pavani@gmail.com>
2024-03-13 21:58:01 +0530

    keep the code in a common file

--- expense_backend.sh
+++ expense_backend.sh
@@ -31,7 +31,7 @@ print_status $?
 #print_heading "clean old content"
 #rm -rf /app &>>$LOG
 #print_status $?
-
+App_task_code
 print_heading "make directory app"
 mkdir /app &>>$LOG
 print_status $?
@@ -40,14 +40,14 @@ print_status $?
 #print_heading "Download backend code"
 #curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$LOG
 #print_status $?
-App_task_code
+
 print_heading "Move to app directory"
 cd ${app_dir} &>>LOG
 print_status $?
 
-#print_heading "unzip backend code"
-#unzip /tmp/backend.zip &>>$LOG
-#print_status $?
+print_heading "unzip backend code"
+unzip /tmp/backend.zip &>>$LOG
+print_status $?
 
 #print_heading "move to app directory"
 #cd /app &>>$LOG
