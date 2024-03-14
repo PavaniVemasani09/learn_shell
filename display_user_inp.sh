1f246b60b5d41e47c7002aa6a70301b558018744
Pavani <vemasani.pavani@gmail.com>
2024-03-14 13:54:13 +0530

    prompt the message along with the input

--- display_user_inp.sh
+++ display_user_inp.sh
@@ -1,4 +1,12 @@
-echo "Enter the number:"
-read num
-echo "The number is : $num"
-echo "The number is :"  +$num
+#Read the input by using "read" command and store the value into a variable "num"
+#echo "Enter the number:"
+#read num
+#echo "The number is : $num"
+#echo "The number is :"  +$num
+
+#*****************************#
+
+#Prompt the message along with the read command
+read -p "Enter the number:" num
+echo $num
+echo ${num}
