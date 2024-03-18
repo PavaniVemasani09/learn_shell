57826042cbb9058c79764cdc145b2205766ecbb9
Pavani <vemasani.pavani@gmail.com>
2024-03-19 05:24:06 +0530

    if else condition

--- ArithmeticOp.sh
+++ ArithmeticOp.sh
@@ -7,5 +7,16 @@
 #echo $(($num1 + $num2))
 
 
-read -p "Enter number :" num1 num2
-echo $(($num1+$num2))
+#read -p "Enter number :" num1 num2
+#echo $(($num1+$num2))
+
+read -p "Enter two values:" num1,num2
+read -p "Enter Operator:" op
+if [ op == + ] then
+  echo $(($num1+$num2))
+
+elif [ op == -] then
+echo $(($num1-$num2))
+
+else
+  echo "Operator not listed"
