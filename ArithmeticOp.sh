5a6330d0b75127fff0016a6bb82cc35285f554b2
Pavani <vemasani.pavani@gmail.com>
2024-03-19 05:17:52 +0530

    read variables in the same line

--- ArithmeticOp.sh
+++ ArithmeticOp.sh
@@ -2,7 +2,10 @@
 #b=20
 #echo $(($a+$b))
 
-read -p "Enter number:" num1
-read -p "Enter number2:" num2
-echo $(($num1 + $num2))
+#read -p "Enter number:" num1
+#read -p "Enter number2:" num2
+#echo $(($num1 + $num2))
 
+
+read -p "Enter number :" num1 num2
+echo $(($num1_$num2))
