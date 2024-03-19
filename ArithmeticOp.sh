6648414ef37a8834f2e87bb0c554ae900a56d13f
Pavani <vemasani.pavani@gmail.com>
2024-03-19 05:31:59 +0530

    parsing values from command prompt

--- ArithmeticOp.sh
+++ ArithmeticOp.sh
@@ -10,14 +10,23 @@
 #read -p "Enter number :" num1 num2
 #echo $(($num1+$num2))
 
-read -p "Enter two values:" num1 num2
-read -p "Enter Operator:" op
-if [ $op == + ]
+#read -p "Enter two values:" num1 num2
+#read -p "Enter Operator:" op
+#if [ $op == + ]
+#then
+#echo $(($num1+$num2))
+#elif [ $op == - ]
+#then
+#echo $(($num1-$num2))
+#else
+#echo "Operator not listed"
+#fi
+
+
+read -p "Enter number : " num
+if [ num%2 == 0 ]
 then
-echo $(($num1+$num2))
-elif [ $op == - ]
-then
-echo $(($num1-$num2))
+echo "Even number"
 else
-echo "Operator not listed"
+echo "not a even number"
 fi
